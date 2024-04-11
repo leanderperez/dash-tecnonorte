17c6fbe503ab6a587f9a887ba76acfa344befe14
leanderperez <leanderperez@gmail.com>
2024-04-11 16:55:53 -0400

    11/04/24

--- Concat.py
+++ Concat.py
@@ -8,14 +8,14 @@ import pandas as pd
 
 # Crea los DataFrames
 df1 = pd.read_excel("Consol.xlsx")
-df2 = pd.read_excel("ivan.xlsx")
-df3 = pd.read_excel("octavio.xlsx")
+df2 = pd.read_excel("Consol_plus042024.xlsx")
+# df3 = pd.read_excel("octavio.xlsx")
 
 
 # Concatena los DataFrames y elimina los duplicados
-df4 = pd.concat([df1, df2, df3], axis=0)
+df4 = pd.concat([df1, df2], axis=0)
 df4 = df4.drop_duplicates(subset=["REFERENCIA", "REPORTE", "FECHA DE REPORTE"], keep="first")
 df4 = df4.fillna('')
 
 # Crea un nuevo Excel
-df4.to_excel("new_consol2.xlsx")
+df4.to_excel("new_consol.xlsx")
